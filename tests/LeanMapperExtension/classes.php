1b5fefb8cffbd1abda4b958b4f9a7895cf530714
Jan Pecha <janpecha@email.cz>
2017-04-29 16:14:13 +0200

    Updated version constraints

--- tests/LeanMapperExtension/classes.php
+++ tests/LeanMapperExtension/classes.php
@@ -1,8 +1,24 @@
 <?php
 
-class DibiFakeMySqlDriver extends DibiMySqlDriver
-{
-	public function __construct()
-	{
+namespace {
+	if (class_exists('DibiMySqlDriver')) {
+		class DibiFakeMySqlDriver extends DibiMySqlDriver
+		{
+			public function __construct()
+			{
+			}
+		}
+	}
+}
+
+
+namespace Dibi\Drivers {
+	if (class_exists('Dibi\Drivers\MySqlDriver')) {
+		class FakeMySqlDriver extends MySqlDriver
+		{
+			public function __construct()
+			{
+			}
+		}
 	}
 }
